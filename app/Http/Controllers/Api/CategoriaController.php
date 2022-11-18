2b9aabe69ca17b1c23131402a041528fe9bb6551
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-11-18 06:49:36 -0400

    Trabajando con Eloquent en los Modelos

--- app/Http/Controllers/Api/CategoriaController.php
+++ app/Http/Controllers/Api/CategoriaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
@@ -10,30 +11,70 @@ class CategoriaController extends Controller
 
     public function index()
     {
-        //
+        //listar toda la informacion
+        $lista_categorias = Categoria::all();
+        return response()->json($lista_categorias, 200);
     }
 
-
+    // Guardar
     public function store(Request $request)
     {
-        //
+        // validar antes de guardar
+        $request->validate([
+            "nombre" => "required|unique:categorias|max:50|min:3",
+        ]);
+
+        // guardar
+        $cat = new Categoria();
+        $cat->nombre = $request->nombre;
+        $cat->detalle = $request->detalle;
+        $cat->save();
+
+        return response()->json(["mensaje" => "Categoria Registrado", "data" => $cat], 201);
     }
 
-
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
-        //
+        // $categoria = Categoria::findorFail($id);
+        $categoria = Categoria::find($id);
+        if ($categoria) return response()->json($categoria, 200);
+        return response()->json(["mensaje" => "No se encontro la categoria"], 404);
     }
 
-
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            // Verficamos que se pueda guardar los camios en el id especicado
+            "nombre" => "required|max:50|min:3|unique:categorias,nombre,$id",
+        ]);
+
+        $categoria = Categoria::where("id", $id)->first();
+        if ($categoria) {
+            $categoria->nombre = $request->nombre;
+            $categoria->detalle = $request->detalle;
+            $categoria->save();
+
+            return response()->json(["mensaje" => "Categoria Modificada", "data" => $categoria], 201);
+        }
+        return response()->json(["mensaje" => "No se encontro la categoria"], 404);
     }
 
 
     public function destroy($id)
     {
-        //
+        // Clase dia 4 hora 1: Minuto 12
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,28 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\ProductoController;
+use App\Http\Controllers\Api\ClienteController;
+use App\Http\Controllers\Api\PedidoController;
+use App\Http\Controllers\Api\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Agregando Rutas (apiResource)
+/*
+    GET /api/categoria              (index -> listar)
+    POST /api/categoria             (store -> guardar)
+    GET /api/categoria/{id}         (show -> mostrar)
+    PUT /api/categoria/{id}         (update -> modificar)
+    DELETE /api/categoria/{id}      (destroy -> eliminar)
+*/
+Route::apiResource("categoria", CategoriaController::class);
+Route::apiResource("producto", ProductoController::class);
+Route::apiResource("cliente", ClienteController::class);
+Route::apiResource("pedido", PedidoController::class);
+Route::apiResource("usuario", UsuarioController::class);
