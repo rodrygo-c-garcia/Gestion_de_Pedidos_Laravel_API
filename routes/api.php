415855ea64693eed9cf991b23ab0d99fe99dd626
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-11-29 16:05:12 -0400

    feat: add controller auth and his configuration
    controlador auth y su configuracion

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,17 @@ Route::apiResource("producto", ProductoController::class);
 Route::apiResource("cliente", ClienteController::class);
 Route::apiResource("pedido", PedidoController::class);
 Route::apiResource("usuario", UsuarioController::class);
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
