7be569f088c60f4fe180263cf415935473a4c910
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-12-10 20:54:32 -0400

    feat: add functions in of controllerSanctum

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function login()
+    {
+    }
+    public function registro()
+    {
+    }
+    public function perfil()
+    {
+    }
+    public function logout()
+    {
+    }
 }
