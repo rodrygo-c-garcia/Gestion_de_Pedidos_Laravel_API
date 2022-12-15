577ca8d40dfc5967ca6880cc9c7fc8f42a9c1999
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-12-15 15:01:11 -0400

    feat (SanctumController): add the logic register users

--- app/Http/Controllers/SanctumAuthController.php
+++ app/Http/Controllers/SanctumAuthController.php
@@ -2,19 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class SanctumAuthController extends Controller
 {
+    // registrar usuario
     public function login(Request $request)
     {
     }
+
     public function registro(Request $request)
     {
+        // 1 -> validar
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed",
+        ]);
+        // 2 -> guardar
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json(["mensaje" => "Usuario Registrado"], 201);
     }
+
     public function perfil()
     {
     }
+
     public function logout()
     {
     }
