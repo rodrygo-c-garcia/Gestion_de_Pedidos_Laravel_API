ec37de1b4042ec777f57e107e731f781c5214d71
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-20 14:29:11 -0400

    feat: add function update in the Product

--- app/Http/Controllers/Api/ProductoController.php
+++ app/Http/Controllers/Api/ProductoController.php
@@ -51,4 +51,36 @@ class ProductoController extends Controller
         $producto = Producto::find($id);
         return response()->json($producto, 200);
     }
+
+    public function update(Request $request, $id)
+    {
+        // validar antes de guardar
+        $request->validate([
+            // que verifique todo excepto el dato seleccionado
+            "nombre" => "required|max:20|min:3|unique:productos,nombre,$id",
+            'precio' => "required",
+            'categoria_id' => "required"
+        ]);
+
+        $producto = Producto::find($id);
+
+
+        // actualizamos los datos del Producto
+        $producto = new Producto();
+        $producto->nombre = $request->nombre;
+        $producto->precio = $request->precio;
+        $producto->stock = $request->stock;
+        $producto->descripcion = $request->descripcion;
+        $producto->estado = $request->estado;
+        $producto->categoria_i = $request->categoria_i;
+        $name_img = '';
+        if ($file = $request->file('imagen')) {
+            $name_img = time() . "-" . $file->getClientOriginalName();
+            $file->move('imagenes');
+            $producto->imagen = '/imagenes' . $name_img;
+        }
+        $producto->save();
+
+        return response()->json(["mensaje" => "Producto Modificado"], 201);
+    }
 }
