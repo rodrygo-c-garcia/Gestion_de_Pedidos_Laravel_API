52aa5b242fb98f9b96da19438235363afab5da10
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-01-31 15:45:54 -0400

    feat: add the function store Cliente

--- app/Http/Controllers/Api/ClienteController.php
+++ app/Http/Controllers/Api/ClienteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cliente;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
@@ -16,7 +17,20 @@ class ClienteController extends Controller
 
     public function store(Request $request)
     {
-        //
+        //validamos
+        $request->validate([
+            'nombreCompleto' => 'required',
+        ]);
+
+        // creamos nuevo Cliente
+        $cliente = new Cliente();
+        $cliente->nombreCompleto = $request->nombreCompleto;
+        $cliente->ci_nit = $request->ci_nit;
+        $cliente->telefono = $request->telefono;
+        $cliente->correo = $request->correo;
+        $cliente->save();
+
+        return response()->json(['mensaje' => 'Cliente registrado', 'cliente' => $cliente]);
     }
 
 
